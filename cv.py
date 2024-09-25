1a7234c634d11c9dcf1c19e297b66c8669a6eda3
JiSukh <jsukharom1999@gmail.com>
2024-09-25 04:24:12 +0100

    basic face classifier with opencv's HAAR_classifier

--- cv.py
+++ cv.py
@@ -4,9 +4,18 @@ cam = cv2.VideoCapture(0)
 
 while True:
     ret,frame = cam.read()
-    cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) and 0xFF == ord('a'):
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    haar = cv2.CascadeClassifier('haar_face.xml')
+
+    faces = haar.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), thickness=1)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('a'):
         break
 
 cv2.destroyAllWindows()
